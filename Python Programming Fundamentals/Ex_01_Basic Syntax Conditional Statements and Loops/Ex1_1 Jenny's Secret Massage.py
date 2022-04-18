12b32c0adf674f0456f4bc1ca51f8a4b5a7212f4
Radoslav Bukov <102231498+RadoslavBukov@users.noreply.github.com>
2022-04-18 15:02:11 +0300

    Add files via upload

---
+++ Python Programming Fundamentals/Ex_01_Basic Syntax Conditional Statements and Loops/Ex1_1 Jenny's Secret Massage.py
@@ -0,0 +1,14 @@
+"""
+Input       	Output
+
+Peter       	Hello, Peter!
+Amy	            Hello, Amy!
+Johnny	        Hello, my love!
+"""
+
+name = (input())
+
+if name == "Johnny":
+    print('Hello, my love!')
+else:
+    print(f'Hello, {name}!')
