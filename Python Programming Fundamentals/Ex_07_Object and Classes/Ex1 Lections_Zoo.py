aed334eb4c50a817f8112f13262b7e7c6602ef99
Radoslav Bukov <radoslaw.bukow@gmail.com>
2022-05-23 09:10:19 +0300

    Add files via upload

---
+++ Python Programming Fundamentals/Ex_07_Object and Classes/Ex1 Lections_Zoo.py
@@ -0,0 +1,58 @@
+"""
+Input	                    Output
+Great Zoo                   Mammals in Great Zoo: lion, bear, tiger
+5                           Total animals: 5
+mammal lion
+mammal bear
+fish salmon
+bird owl
+mammal tiger
+mammal
+
+Blah                        Mammals in Blah: bear
+1                           Total animals: 1
+mammal bear
+mammal
+"""
+class Zoo:
+    __animals = 0
+    def __init__(self,name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+
+    def add_animal(self, species, name):
+        if species == 'mammal':
+            self.mammals.append(name)
+        elif species == 'fishes':
+            self.fishes.append(name)
+        elif species == 'mammal':
+            self.birds.append(name)
+
+        Zoo.__animals += 1
+
+    def get_info(self, species):
+        result = ''
+        if species == 'mammal':
+            result += f"Mammals in {self.name}: {', '.join(self.mammals)}"
+        elif species == 'fishes':
+            result += f"Fishes in {self.name}: {', '.join(self.fishes)}"
+        elif species == 'bird':
+            result += f"Birds in {self.name}: {', '.join(self.birds)}"
+
+        result += f"\nTotal animals: {Zoo.__animals}"
+        return result
+
+name_of_zoo = input()
+zoo = Zoo(name_of_zoo)
+number_of_lines = int(input())
+
+for i in range(number_of_lines):
+    info = input().split(' ')
+    species = info[0]
+    type_of_animal = info[1]
+    zoo.add_animal(species, type_of_animal)
+
+additional_info = input()
+print(zoo.get_info(additional_info))
