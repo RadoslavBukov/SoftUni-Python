12f8b8c84f41703240d023318aee1bfc25958f13
Radoslav Bukov <radoslaw.bukow@gmail.com>
2022-07-29 09:43:50 +0300

    Add files via upload

---
+++ Python Programming Advanced/Ex_05_Functions Advanced/Ex05_05 Concatenate.py
@@ -0,0 +1,16 @@
+"""
+Test Code	                                                                                Output
+
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))	            SoftUniIsGreat!
+
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))	            I Love Python
+"""
+def concatenate(*args,**kwargs):
+    text = "".join(args)
+
+    for key, value in kwargs.items():
+        text = text.replace(key, value)
+
+    return text
+
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
