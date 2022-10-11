70ac9a1f1b048f8fa87a8c8884efeaf0a49d4db4
Radoslav Bukov <radoslaw.bukow@gmail.com>
2022-10-11 09:03:02 +0300

    Add files via upload

---
+++ Python Programming OOP/Ex_03_Inheritance/Ex_05_project/food.py
@@ -0,0 +1,8 @@
+from Ex_05_project.product import Product
+
+class Food(Product):
+    DEFAULT_QUANTITY = 15
+
+    def __init__(self, name):
+        super().__init__(name, self.DEFAULT_QUANTITY)
+
