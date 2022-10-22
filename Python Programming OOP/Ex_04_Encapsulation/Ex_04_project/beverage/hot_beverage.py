93c6716754e6f6b958763d2250705c13eaf6d03f
Radoslav Bukov <radoslaw.bukow@gmail.com>
2022-10-22 17:12:31 +0300

    Add files via upload

---
+++ Python Programming OOP/Ex_04_Encapsulation/Ex_04_project/beverage/hot_beverage.py
@@ -0,0 +1,7 @@
+from Ex_04_project.beverage.beverage import Beverage
+
+class HotBeverage(Beverage):
+
+    def __init__(self, name, price, milliliters):
+        super().__init__(name, price, milliliters)
+
