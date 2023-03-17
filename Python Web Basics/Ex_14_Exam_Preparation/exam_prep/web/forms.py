3e79abb08e559df8b0e882bfbd327101bc5795c1
Radoslav Bukov <radoslaw.bukow@gmail.com>
2023-03-17 10:00:17 +0200

    Add files via upload

---
+++ Python Web Basics/Ex_14_Exam_Preparation/exam_prep/web/forms.py
@@ -0,0 +1,85 @@
+from django import forms
+
+from exam_prep.web.form_mixins import DisabledFormMixin
+from exam_prep.web.models import Profile, Album
+
+
+class ProfileBaseForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class ProfileCreateForm(ProfileBaseForm):
+    pass
+
+
+class ProfileDeleteForm(ProfileBaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_hidden_fields()
+
+    def save(self, commit=True):
+        if commit:
+            Album.objects.all().delete()
+            self.instance.delete()
+        return self.instance
+
+    def __set_hidden_fields(self):
+        for _, field in self.fields.items():
+            field.widget = forms.HiddenInput()
+
+
+class AlbumBaseForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = '__all__'
+        widgets = {
+            'album_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Album Name',
+                }
+            ),
+            'artist': forms.TextInput(
+                attrs={
+                    'placeholder': 'Artist',
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'placeholder': 'Description',
+                }
+            ),
+            'image_url': forms.URLInput(
+                attrs={
+                    'placeholder': 'Image URL',
+                }
+            ),
+            'price': forms.NumberInput(
+                attrs={
+                    'placeholder': 'Price',
+                }
+            ),
+        }
+
+
+class AlbumCreateForm(AlbumBaseForm):
+    pass
+
+
+class AlbumEditForm(AlbumBaseForm):
+    pass
+
+
+class AlbumDeleteForm(DisabledFormMixin, AlbumBaseForm):
+    disabled_fields = ('album_name', 'artist', 'description', 'image_url', 'parice')
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._disable_fields()
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+        return self.instance
